e60d3132b77c7d13ef494a257f1bc3c6817f5172
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-16 00:54:39 +0600

    Added Payment method and functionalities

---
+++ src/pages/subscription/CheckoutForm.jsx
@@ -0,0 +1,182 @@
+import PropTypes from "prop-types";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { useSecureAPI } from "../../hooks/useAPI_Links";
+import useContextValue from "../../hooks/useContextValue";
+
+const CheckoutForm = ({ total_price = 0 }, handleModal) => {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [clientSecret, setClientSecret] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const [isCopyTrx, setIsCopyTrx] = useState(false);
+  const [trxId, setTrxId] = useState(null);
+  const stripe = useStripe();
+  const elements = useElements();
+  const secureAPI = useSecureAPI();
+  const { user } = useContextValue();
+
+  useEffect(() => {
+    const getStripeSecret = async () => {
+      if (total_price <= 0) return setClientSecret(null);
+      setProcessing(true);
+      try {
+        const { data } = await secureAPI.post("/create-payment-intent", {
+          price: total_price,
+        });
+        setClientSecret(data.clientSecret);
+        setErrorMessage(null);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setProcessing(false);
+      }
+    };
+    getStripeSecret();
+  }, [secureAPI, total_price]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setErrorMessage(null);
+    setProcessing(true);
+
+    if (!stripe || !elements) {
+      setProcessing(false);
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (!card) {
+      setProcessing(false);
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) setErrorMessage(error.message);
+    else console.log("[paymentMethod]", paymentMethod);
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      setProcessing(false);
+      setErrorMessage(error.message);
+    } else {
+      //   console.log("[paymentIntent]", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        // save the payment history in the database
+        const history = {
+          email: user?.email,
+          price: total_price,
+          trx_id: paymentIntent.id,
+        };
+
+        const { data } = await secureAPI.post("/subscription-histories", history);
+        console.log(data);
+        // if (
+        //   data?.deleteOrders?.deletedCount > 0 &&
+        //   data?.paymentHistory?.insertedId
+        // ) {
+        //   handleModal(false);
+        //   setProcessing(false);
+        //   setTrxId(paymentIntent.id);
+        //   Swal.fire({
+        //     title: "Payment Successful!",
+        //     text: "Thank you for your purchase!",
+        //     icon: "success",
+        //     showConfirmButton: false,
+        //     position: "center",
+        //     timer: 2000,
+        //   });
+        // }
+      }
+    }
+  };
+
+  const handleCopyTrxId = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigator.clipboard.writeText(trxId);
+    setIsCopyTrx(true);
+    setTimeout(() => {
+      setIsCopyTrx(false);
+    }, 2000);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col space-y-4 bg-white p-2 rounded-lg mx-auto"
+    >
+      <div className="border border-gray-300 p-3 rounded-md">
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                fontFamily: "sans-serif",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+          className="StripeElement"
+        />
+      </div>
+      {processing && (
+        <progress className="progress progress-info  w-full "></progress>
+      )}
+      <button
+        type="submit"
+        disabled={!stripe || !elements || !clientSecret || processing || trxId}
+        className="bg-info text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-500  active:scale-[99%] active:translate-y-[1px] disabled:bg-gray-400 disabled:cursor-not-allowed disabled:active:scale-100 disabled:active:translate-y-0 transition-colors duration-300"
+      >
+        Pay
+      </button>
+      {errorMessage && (
+        <p className="text-error normal-case text-sm">{errorMessage}</p>
+      )}
+      {trxId && (
+        <div className="normal-case text-sm cursor-default space-y-2 relative">
+          <p>Your payment was successful. Thank you for subscription!</p>
+          <p>
+            Your TrxID is:{" "}
+            <button onClick={(e) => handleCopyTrxId(e)} className="text-info">
+              {trxId}{" "}
+              {isCopyTrx && (
+                <span className="bg-slate-600 px-1 py-[2px] text-white rounded">
+                  Copied!
+                </span>
+              )}
+            </button>
+          </p>
+        </div>
+      )}
+    </form>
+  );
+};
+
+CheckoutForm.propTypes = {
+  total_price: PropTypes.number.isRequired,
+  handleModal: PropTypes.func,
+};
+
+export default CheckoutForm;
--- src/pages/subscription/Subscription.jsx
+++ src/pages/subscription/Subscription.jsx
@@ -1,31 +1,98 @@
-// import Lottie from "lottie-react";
-// import lottie from "../../assets/lottie/subscription.json";
-
 import banner from "../../assets/images/subscription-banner.jpg";
 import mobileBanner from "../../assets/images/mobile_subscription_banner.jpg";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { BsCurrencyDollar } from "react-icons/bs";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
+
 const Subscription = () => {
+  const [price, setPrice] = useState(0);
+
+  // handle modal
+  const handleModal = (action) => {
+    const modal = document.getElementById("my_modal_5");
+    if (action) modal.showModal();
+    else modal.close();
+  };
+
+
   return (
-    <div className="relative max-w-screen-2xl mx-auto flex md:items-center justify-center md:justify-start">
-      <div className="hidden md:block">
-        <img className="w-full" src={banner} alt="" />
+    <>
+      {" "}
+      <div className="relative max-w-screen-2xl mx-auto flex md:items-center justify-center md:justify-start">
+        <div className="hidden md:block">
+          <img className="w-full" src={banner} alt="" />
+        </div>
+        <div className="md:hidden">
+          <img className="w-full" src={mobileBanner} alt="" />
+        </div>
+        <div className="absolute px-5 md:px-10 max-w-96 md:max-w-[450px] lg:max-w-[600px] pt-36 md:pt-0 text-center md:text-left">
+          <h2 className="text-2xl lg:text-4xl font-bold font-davidLibre mb-2">
+            Unlock Exclusive Insights with <br /> Pixel News Premium
+          </h2>
+          <p className="text-sm">
+            Elevate your news experience with our Premium Subscription. Access
+            exclusive articles, in-depth analysis, and ad-free browsing to stay
+            informed and ahead of the curve.
+          </p>
+          <button
+            onClick={() => handleModal(true)}
+            className="bg-gradient-to-r from-[#00D7FF] to-[#E7ABFF] rounded-full mt-5 shadow-md px-3 md:px-10 py-1 md:py-3 text-white font-semibold md:font-bold md:text-xl uppercase active:scale-[98%] active:translate-y-[1px]"
+          >
+            Get Subscription
+          </button>
+        </div>
       </div>
-      <div className="md:hidden">
-        <img className="w-full" src={mobileBanner} alt="" />
-      </div>
-      <div className="absolute px-5 md:px-10 max-w-96 md:max-w-[450px] lg:max-w-[600px] pt-36 md:pt-0 text-center md:text-left">
-        <h2 className="text-2xl lg:text-4xl font-bold font-davidLibre mb-2">
-          Unlock Exclusive Insights with <br /> Pixel News Premium
-        </h2>
-        <p className="text-sm">
-          Elevate your news experience with our Premium Subscription. Access
-          exclusive articles, in-depth analysis, and ad-free browsing to stay
-          informed and ahead of the curve.
-        </p>
-        <button className="bg-gradient-to-r from-[#00D7FF] to-[#E7ABFF] rounded-full mt-5 shadow-md px-3 md:px-10 py-1 md:py-3 text-white font-semibold md:font-bold md:text-xl uppercase active:scale-[98%] active:translate-y-[1px]">
-          Get Subscription
-        </button>
-      </div>
-    </div>
+      <dialog
+        id="my_modal_5"
+        className="modal modal-bottom sm:modal-middle md:w-[600px] mx-auto"
+      >
+        <div className="modal-box">
+          <div className="relative">
+            <button
+              onClick={() => handleModal(false)}
+              className="absolute -right-5 -top-5 btn btn-sm btn-circle text-black"
+            >
+              <IoClose />
+            </button>
+          </div>
+          <h4 className="text-center font-girassol text-3xl mb-2">Payment</h4>
+          <div className="p-2 flex gap-x-3">
+            <select
+              onChange={(e) => setPrice(parseInt(e.target.value))}
+              className="select select-bordered w-full max-w-72"
+            >
+              <option className="hidden" value={0}>
+                Select a Period
+              </option>
+              <option value={2}>1 minute</option>
+              <option value={15}>5 days</option>
+              <option value={25}>10 days</option>
+            </select>
+            <label className="relative flex items-center">
+              <input
+                type="text"
+                value={price}
+                readOnly
+                className="input input-bordered w-full max-w-xs pl-7 text-lg"
+              />
+              <span className="absolute left-3 text-lg">
+                <BsCurrencyDollar />
+              </span>
+            </label>
+          </div>
+          <div>
+            <Elements stripe={stripePromise}>
+              <CheckoutForm total_price={price} handleModal={handleModal} />
+            </Elements>
+          </div>
+        </div>
+      </dialog>
+    </>
   );
 };
 
