a9361f8ad809ad61d4d21cdaf3e9fc1cf5bfca21
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-16 13:14:59 +0600

    Updated PremiumBadge component

--- src/components/badges/PremiumBadge.jsx
+++ src/components/badges/PremiumBadge.jsx
@@ -3,8 +3,13 @@ import lottie from "../../assets/lottie/premium.json";
 
 const PremiumBadge = () => {
   return (
-    <div className="absolute left-3 top-3 w-12 h-12 overflow-hidden rounded-full shadow-xl">
-      <Lottie animationData={lottie} />
+    <div className="absolute left-3 top-3 w-12 h-12 rounded-full flex items-center justify-center">
+      <div className='relative w-12 h-12 flex items-center justify-center'>
+      <div className="absolute w-12 h-12 z-10 "><Lottie animationData={lottie} /></div>
+      <div className="absolute w-6 h-6 bg-[#984AFF] animate-ping blur"></div>
+      <div className="absolute w-6 h-6 bg-[#984AFF] animate-ping blur"></div>
+
+      </div>
     </div>
   );
 };
