81213dd23ca332770f93f45102c00b83888d0e17
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-18 02:21:34 +0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      events: "events",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000,
+  },
+  optimizeDeps: {
+    include: ["events"],
+  },
+});
