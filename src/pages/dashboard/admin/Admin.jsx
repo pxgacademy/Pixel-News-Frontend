69655f43aa97bd93c901fe57432d83f659b53fe3
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-19 20:48:07 +0600

    Added PieChart functionalities

--- src/pages/dashboard/admin/Admin.jsx
+++ src/pages/dashboard/admin/Admin.jsx
@@ -1,14 +1,40 @@
 import DashboardContainer from "../../../components/container/DashboardContainer";
-
-
+import { Chart } from "react-google-charts";
+import { useSecureDataLoader } from "../../../hooks/useDataLoader";
+import Loading from "../../../components/loading/Loading";
+import useContextValue from "../../../hooks/useContextValue";
 
 const Admin = () => {
-    return (
-        <DashboardContainer header='Admin Dashboard' >
+  const { loading } = useContextValue();
+  const [data, isLoading] = useSecureDataLoader("/admin/analytics");
 
-            <h5>Admin</h5>
-        </DashboardContainer>
-    );
+  const count =
+    data?.articlesPerPublisher?.map((value) => [
+      value._id,
+      value.totalArticles,
+    ]) || [];
+  console.log(count);
+
+  const tata = [["Publisher's name", "Total articles"], ...count];
+
+  const options = {
+    title: "Publisher's Percentage ",
+    legend: { position: "left" },
+    pieHole: 0.2,
+  };
+
+  if (isLoading || loading) return <Loading />;
+  return (
+    <DashboardContainer header="Admin Dashboard">
+      <Chart
+        chartType="PieChart"
+        data={tata}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+    </DashboardContainer>
+  );
 };
 
 export default Admin;
