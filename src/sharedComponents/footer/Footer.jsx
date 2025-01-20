f34f87916ac032f53304ed79cc1ac55ec68633d3
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-20 21:54:09 +0600

    Updated footer section

--- src/sharedComponents/footer/Footer.jsx
+++ src/sharedComponents/footer/Footer.jsx
@@ -11,44 +11,51 @@ const Footer = () => {
           </p>
         </div>
         <section className="footer w-full max-w-7xl mx-auto text-white">
-          <nav>
-            <h6 className="footer-title">Services</h6>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
-          </nav>
-          <nav>
-            <h6 className="footer-title">Company</h6>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
-          </nav>
-          <nav>
-            <h6 className="footer-title">Legal</h6>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
-          </nav>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <h6 className="footer-title">Newsletter</h6>
-            <fieldset className="form-control w-80">
-              <label className="label">
-                <span className="label-text text-white">
-                  Enter your email address
-                </span>
-              </label>
-              <div className="join">
-                <input
-                  type="text"
-                  placeholder="username@site.com"
-                  className="input input-bordered join-item"
-                />
-                <button className="btn btn-primary join-item">Subscribe</button>
-              </div>
-            </fieldset>
-          </form>
+          <div className="flex justify-between w-full">
+            <nav className="flex flex-col">
+              <h6 className="footer-title">Services</h6>
+              <a className="link link-hover">Branding</a>
+              <a className="link link-hover">Design</a>
+              <a className="link link-hover">Marketing</a>
+              <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav className="flex flex-col">
+              <h6 className="footer-title">Company</h6>
+              <a className="link link-hover">About us</a>
+              <a className="link link-hover">Contact</a>
+              <a className="link link-hover">Jobs</a>
+              <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav className="flex flex-col">
+              <h6 className="footer-title">Legal</h6>
+              <a className="link link-hover">Terms of use</a>
+              <a className="link link-hover">Privacy policy</a>
+              <a className="link link-hover">Cookie policy</a>
+            </nav>
+          </div>
+
+          <div className="flex justify-center w-full">
+            <form onSubmit={(e) => e.preventDefault()}>
+              <h6 className="footer-title">Newsletter</h6>
+              <fieldset className="form-control w-80">
+                <label className="label">
+                  <span className="label-text text-white">
+                    Enter your email address
+                  </span>
+                </label>
+                <div className="join">
+                  <input
+                    type="text"
+                    placeholder="username@site.com"
+                    className="input input-bordered join-item text-darkTwo"
+                  />
+                  <button className="btn btn-primary join-item">
+                    Subscribe
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
         </section>
       </section>
 
